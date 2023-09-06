1cff0b0050df8b482ec36fb00b69d97f8c7f86aa
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2023-09-05 21:40:32 -0700

    genetrating accuracies results

--- problem1-plots/src/bar-plot/dataprocessing.js
+++ problem1-plots/src/bar-plot/dataprocessing.js
@@ -1,5 +1,19 @@
 export function calculateAccuracies(confusionMatrices) {
+    let accuracies = [];
 
+    for (let matrix of confusionMatrices) {
+        let TP = matrix[0][0];
+        let FP = matrix[0][1];
+        let FN = matrix[1][0];
+        let TN = matrix[1][1];
+
+        let class1Accuracy = TP / (TP + FN);
+        let class2Accuracy = TN / (TN + FP);
+
+        accuracies.push([class1Accuracy, class2Accuracy]);
+    }
+
+    return accuracies;
 }
 
 export function getChartData(confusionMatrices) {
